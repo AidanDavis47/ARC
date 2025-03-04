6ce4e6942c66235c95f5e5bac9cd9757239764bc
Aidan <tutloamd@gmail.com>
2025-03-04 12:36:29 -0800

    Initial Commit

---
+++ ARCTests/ARCTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARCTests.swift
+//  ARCTests
+//
+//  Created by Sarah Remy on 3/4/25.
+//
+
+import Testing
+@testable import ARC
+
+struct ARCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
